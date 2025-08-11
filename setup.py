2f1957fdf557b6e077308fdc6bda7f300348f343
Muhammad Muazzain <90068510+MuhammadMuazzain@users.noreply.github.com>
2025-08-12 03:05:37 +0500

    Commit logic update

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='toy-applied-ml-pipeline',
     version='0.1',
+    # packages=find_packages(exclude=['tests']),
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'boto3',
--- training/split.py
+++ training/split.py
@@ -6,16 +6,13 @@ import pandas as pd
 
 def main():
     train_months = ['2020_10', '2020_11']
+    # test_month = '2020_12'
     test_month = '2020_12'
 
-    # Load latest features for train set
     train_df = pd.concat(
         [io.load_output_df(os.path.join('features', month)) for month in train_months])
 
-    # Load latest features for test set
     test_df = io.load_output_df(os.path.join('features', test_month))
-
-    # Save train and test sets
     component_prefix = 'training/files'
     print(io.save_output_df(train_df, f'{component_prefix}/train'))
     print(io.save_output_df(test_df, f'{component_prefix}/test'))
